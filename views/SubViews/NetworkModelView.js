5f575b1fc4341084594eef29b9498263cba78173
Alex Bigelow <alex.r.bigelow@gmail.com>
2018-09-21 18:00:06 -0600

    Basic handle dragging working

--- views/SubViews/NetworkModelView.js
+++ views/SubViews/NetworkModelView.js
@@ -2,7 +2,7 @@
 import GoldenLayoutView from './GoldenLayoutView.js';
 import SvgViewMixin from './SvgViewMixin.js';
 
-const NODE_SIZE = 25;
+const NODE_SIZE = 30;
 const CURVE_OFFSET = NODE_SIZE * 4;
 
 const OBJECT_PATHS = {
@@ -23,7 +23,7 @@ A${NODE_SIZE},${NODE_SIZE},0,1,1,0,${-NODE_SIZE}`,
   'Edge': ''
 };
 
-const HANDLE_SIZE = 6;
+const HANDLE_SIZE = 10;
 const HANDLE_PATHS = {
   // Arrow for directed handles
   'directed': `\
@@ -63,7 +63,8 @@ class NetworkModelView extends SvgViewMixin(GoldenLayoutView) {
   setup () {
     super.setup();
 
-    this.draggingHandle = false;
+    this.draggingConnection = null;
+    this.handleTarget = null;
 
     this.simulation = d3.forceSimulation();
     for (const [forceName, force] of Object.entries(DEFAULT_FORCES)) {
@@ -116,35 +117,42 @@ class NetworkModelView extends SvgViewMixin(GoldenLayoutView) {
     // Dragging behavior (disabled when handles are being dragged)
     objects.call(d3.drag()
       .on('start', d => {
-        if (!this.draggingHandle) {
-          // disable link, charge, and center forces (keep collision)
-          this.simulation.force('link', null);
-          this.simulation.force('charge', null);
-          // this.simulation.force('center', null);
-          if (!d3.event.active) {
-            this.simulation.alpha(0.1).restart();
-          }
-          d.fx = d.x;
-          d.fy = d.y;
+        // disable link, charge, and center forces (keep collision)
+        this.simulation.force('link', null);
+        this.simulation.force('charge', null);
+        // this.simulation.force('center', null);
+        if (!d3.event.active && !this.draggingConnection) {
+          this.simulation.alpha(0.1).restart();
         }
+        d.fx = d.x;
+        d.fy = d.y;
       }).on('drag', d => {
-        if (!this.draggingHandle) {
-          d.fx = d3.event.x;
-          d.fy = d3.event.y;
-        }
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
       }).on('end', d => {
-        if (!this.draggingHandle) {
-          // re-enable link, charge, and center forces
-          this.simulation.force('link', DEFAULT_FORCES.link);
-          this.simulation.force('charge', DEFAULT_FORCES.charge);
-          // this.simulation.force('center', DEFAULT_FORCES.center);
-          if (!d3.event.active) {
-            this.simulation.alpha(0);
-          }
-          delete d.fx;
-          delete d.fy;
+        // re-enable link, charge, and center forces
+        this.simulation.force('link', DEFAULT_FORCES.link);
+        this.simulation.force('charge', DEFAULT_FORCES.charge);
+        // this.simulation.force('center', DEFAULT_FORCES.center);
+        if (!d3.event.active) {
+          this.simulation.alpha(0);
         }
+        delete d.fx;
+        delete d.fy;
       }));
+    // When dragging handles, register objects as targets
+    const self = this;
+    objects.on('mouseenter', function (d) {
+      if (self.draggingConnection) {
+        self.handleTarget = d.classId;
+        d3.select(this).classed('targeted', true);
+      }
+    }).on('mouseleave', function (d) {
+      if (self.draggingConnection) {
+        self.handleTarget = null;
+        d3.select(this).classed('targeted', false);
+      }
+    });
 
     // Diamond, circle, or line
     objectsEnter.append('path').classed('objectShape', true);
@@ -223,6 +231,9 @@ L${d.x + offset + this.emSize},${d.y + this.emSize / 3}`;
       .classed('connection', true);
     connectionLines = connectionLines.merge(connectionLinesEnter);
 
+    connectionLines.classed('targeted', d => d.id === this.draggingConnection);
+
+    // Compute the path (and the handle positions along the way)
     connectionLines.attr('d', d => {
       if (d.source.x === undefined || d.source.y === undefined ||
           d.target.x === undefined || d.target.y === undefined) {
@@ -230,40 +241,48 @@ L${d.x + offset + this.emSize},${d.y + this.emSize / 3}`;
       } else if (d.source.type === 'Node') {
         // Round down to avoid gaps in the path
         const offset = Math.floor(d.target.labelWidth / 2) + this.emSize;
+        // Compute handle and curve anchor point positions
+        d.source.handlePositions[d.id] = d.source.handlePositions[d.id] || {};
         const curveX = d.target.x - offset - CURVE_OFFSET;
         const curveY = d.target.y + this.emSize / 3;
-        const curveTheta = Math.atan2(curveY - d.source.y, curveX - d.source.x);
+        const theta = Math.atan2(curveY - d.source.y, curveX - d.source.x);
+        const x = d.source.x + NODE_SIZE * Math.cos(theta) +
+          (d.source.handlePositions[d.id].dx || 0);
+        const y = d.source.y + NODE_SIZE * Math.sin(theta) +
+          (d.source.handlePositions[d.id].dy || 0);
         // Update the node's outgoing handlePositions
-        d.source.handlePositions[d.id] = {
+        Object.assign(d.source.handlePositions[d.id], {
+          connectionId: d.id,
           edgeClassId: d.target.classId,
-          theta: curveTheta,
-          x: NODE_SIZE * Math.cos(curveTheta),
-          y: NODE_SIZE * Math.sin(curveTheta)
-        };
+          theta,
+          x,
+          y
+        });
         // Return the arc
-        return `\
-M${d.source.x},${d.source.y}\
-Q${curveX},${curveY}\
-,${d.target.x - offset},${curveY}`;
+        return `M${x},${y}Q${curveX},${curveY},${d.target.x - offset},${curveY}`;
       } else {
         // Round down to avoid gaps in the path
         const offset = Math.floor(d.source.labelWidth / 2) + this.emSize;
+        // Compute handle and curve anchor point positions
+        d.target.handlePositions[d.id] = d.target.handlePositions[d.id] || {};
         const curveX = d.source.x + offset + CURVE_OFFSET;
         const curveY = d.source.y + this.emSize / 3;
-        const curveTheta = Math.atan2(curveY - d.target.y, curveX - d.target.x);
+        const theta = Math.atan2(curveY - d.target.y, curveX - d.target.x);
+        const x = d.target.x + NODE_SIZE * Math.cos(theta) +
+          (d.target.handlePositions[d.id].dx || 0);
+        const y = d.target.y + NODE_SIZE * Math.sin(theta) +
+          (d.target.handlePositions[d.id].dy || 0);
         // Update the node's incoming handlePositions
-        d.target.handlePositions[d.id] = {
+        Object.assign(d.target.handlePositions[d.id], {
+          connectionId: d.id,
           edgeClassId: d.source.classId,
-          theta: curveTheta,
           incoming: true,
-          x: NODE_SIZE * Math.cos(curveTheta),
-          y: NODE_SIZE * Math.sin(curveTheta)
-        };
+          theta,
+          x,
+          y
+        });
         // Return the arc
-        return `\
-M${d.source.x + offset},${curveY}\
-Q${curveX},${curveY}\
-,${d.target.x},${d.target.y}`;
+        return `M${d.source.x + offset},${curveY}Q${curveX},${curveY},${x},${y}`;
       }
     });
   }
@@ -277,33 +296,24 @@ Q${curveX},${curveY}\
       .classed('handleGroup', true);
     handleGroups = handleGroups.merge(handleGroupsEnter);
 
-    // Update each group's position
-    handleGroups.attr('transform', d => {
-      if (d.x === undefined || d.y === undefined) {
-        return '';
-      } else {
-        return `translate(${d.x},${d.y})`;
-      }
-    });
-
     // Draw the handles
     let handles = handleGroups.selectAll('.handle')
       .data(d => {
         if (d.type === 'Node') {
           return Object.values(d.handlePositions);
-        } else if (d.type === 'Edge') {
+        } else if (d.type === 'Edge' && d.x !== undefined && d.y !== undefined) {
           return [
             {
               nodeClassId: d.sourceClassId,
               theta: 0,
-              x: -d.labelWidth / 2 - this.emSize,
-              y: this.emSize / 3
+              x: d.x - d.labelWidth / 2 - this.emSize,
+              y: d.y + this.emSize / 3
             },
             {
               nodeClassId: d.targetClassId,
               theta: 0,
-              x: d.labelWidth / 2 + this.emSize,
-              y: this.emSize / 3
+              x: d.x + d.labelWidth / 2 + this.emSize,
+              y: d.y + this.emSize / 3
             }
           ];
         } else {
@@ -315,9 +325,18 @@ Q${curveX},${curveY}\
       .classed('handle', true);
     handles = handles.merge(handlesEnter);
 
-    // Handle circles
-    handlesEnter.append('path');
-    handles.select('path')
+    // Handle halo (to ensure CSS rules don't flicker as we drag)
+    handlesEnter.append('circle')
+      .classed('halo', true)
+      .attr('r', HANDLE_SIZE + 3);
+    handles.select('.halo')
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y);
+
+    // Handle anchors
+    handlesEnter.append('path')
+      .classed('anchor', true);
+    handles.select('.anchor')
       .attr('transform', d => {
         let angle = 180 * d.theta / Math.PI;
         if (d.incoming) {
@@ -326,6 +345,38 @@ Q${curveX},${curveY}\
         return `translate(${d.x},${d.y}) rotate(${angle})`;
       })
       .attr('d', HANDLE_PATHS.directed);
+
+    // Dragging behavior
+    handles.call(d3.drag().on('start', d => {
+      if (!this.draggingConnection) { // allow at most one handle drag in a multi-touch environment
+        this.draggingConnection = d.connectionId;
+        this.handleTarget = null;
+        this.simulation.alpha(0);
+        d.x0 = d3.event.x;
+        d.y0 = d3.event.y;
+      }
+    }).on('drag', d => {
+      d.dx = d3.event.x - d.x0;
+      d.dy = d3.event.y - d.y0;
+      this.drawLineLayer();
+      this.drawHandleLayer();
+    }).on('end', d => {
+      this.draggingConnection = null;
+      delete d.x0;
+      delete d.dx;
+      delete d.y0;
+      delete d.dy;
+      // console.log(this.handleTarget);
+      this.handleTarget = null;
+      this.simulation.alpha(0).restart();
+    }));
+
+    // While dragging handles, we want to ignore pointer events manually
+    // (the :active selector doesn't seem to work as well while dragging)
+    this.handleLayer.style('pointer-events', this.draggingConnection ? 'none' : null);
+    // Similarly, cursor changes mostly get ignored while dragging, so we
+    // need to apply the hidden cursor everywhere
+    this.content.classed('hideCursor', !!this.draggingConnection);
   }
 
   draw () {
