2aefe178fd6af181614dbc640251b2af0937492c
Alex Bigelow <alex.r.bigelow@gmail.com>
2018-11-28 10:48:39 -0700

    Minor menu ui fixes

--- views/MainMenu/ModelMenu/UploadOption.js
+++ views/MainMenu/ModelMenu/UploadOption.js
@@ -6,6 +6,8 @@ class UploadOption extends ModelSubmenuMixin(ModalMenuOption) {
     super(parentMenu, d3el);
     this.icon = 'img/upload.svg';
     this.label = 'Upload Data...';
+    this.loading = false;
+    this.loaded = false;
   }
   setup () {
     super.setup();
@@ -14,14 +16,33 @@ class UploadOption extends ModelSubmenuMixin(ModalMenuOption) {
       .property('multiple', true)
       .style('display', 'none')
       .on('change', () => { this.uploadFiles(); });
-    this.uploadButton = this.contentDiv.append('div')
+    const loadContainer = this.contentDiv.append('div')
+      .classed('loadContainer', true);
+    this.uploadButton = loadContainer.append('div')
       .classed('button', true)
-      .on('click', () => this.uploadInput.node().click());
+      .on('click', () => {
+        if (!this.loading) {
+          this.uploadInput.node().click();
+        }
+      });
     this.uploadButton.append('a');
     this.uploadButton.append('span')
       .text('Choose Files');
+    this.spinner = loadContainer.append('img')
+      .style('display', 'none')
+      .attr('src', 'img/spinner.gif');
+  }
+  draw () {
+    super.draw();
+    this.spinner
+      .style('display', (this.loading || this.loaded) ? null : 'none')
+      .attr('src', this.loading ? 'img/spinner.gif' : 'img/check.svg');
+    this.uploadButton.classed('disabled', this.loading);
   }
   async uploadFiles () {
+    this.loading = true;
+    this.loaded = false;
+    this.render();
     this.loadedFiles = {};
     const fileList = Array.from(this.uploadInput.node().files);
 
@@ -29,6 +50,9 @@ class UploadOption extends ModelSubmenuMixin(ModalMenuOption) {
       await this.model.addFileAsStaticTable({ fileObj });
       window.mainView.render();
     }));
+    this.loading = false;
+    this.loaded = true;
+    this.render();
   }
 }
 export default UploadOption;
